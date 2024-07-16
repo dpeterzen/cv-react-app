4ee3d9621160468299613ab8286a6a31bf0dab60
Dan Peterson <dmpeterson317@gmail.com>
2024-07-16 15:23:03 -0400

    lints and main form styles

--- src/components/EducationInput.jsx
+++ src/components/EducationInput.jsx
@@ -1,52 +1,62 @@
 function EducationInput({ education, updateEducation, deleteEducation }) {
+  const handleSchool = (educationId, e) =>
+    updateEducation(educationId, "school", e.target.value);
 
-  const handleSchool = (educationId, e) => (
-    updateEducation(educationId, 'school', e.target.value)
-  )
+  const handleDegree = (educationId, e) =>
+    updateEducation(educationId, "degree", e.target.value);
 
+  const handleStartDate = (educationId, e) =>
+    updateEducation(educationId, "start", e.target.value);
+
+  const handleEndDate = (educationId, e) =>
+    updateEducation(educationId, "end", e.target.value);
 
   return (
     <>
-      <button className="delete-btn" type="button" onClick={() => deleteEducation(education.id)}>
+      <button
+        className="delete-btn"
+        type="button"
+        onClick={() => deleteEducation(education.id)}
+      >
         Delete
       </button>
-      <label htmlFor={'school-' + education.id}>
+      <label htmlFor={"school-" + education.id}>
         School:
         <input
-          id={'school-' + education.id}
+          id={"school-" + education.id}
           type="text"
           value={education.school}
           onChange={(e) => handleSchool(education.id, e)}
         />
       </label>
 
-      <label htmlFor={'degree-' + education.id}>
+      <label htmlFor={"degree-" + education.id}>
         Degree
         <input
-          id={'degree-' + education.id}
+          id={"degree-" + education.id}
           type="text"
           value={education.degree}
-          onChange={null}
+          onChange={(e) => handleDegree(education.id, e)}
         />
       </label>
 
-      <label htmlFor={'start-' + education.id}>
+      <label htmlFor={"start-" + education.id}>
         Start date:
         <input
-          id={'start-' + education.id}
+          id={"start-" + education.id}
           type="date"
           value={education.start}
-          onChange={null}
+          onChange={(e) => handleStartDate(education.id, e)}
         />
       </label>
 
-      <label htmlFor={'school-' + education.id}>
+      <label htmlFor={"school-" + education.id}>
         End date (or expected):
         <input
-          id={'school-' + education.id}
+          id={"school-" + education.id}
           type="date"
           value={education.school}
-          onChange={null}
+          onChange={(e) => handleEndDate(education.id, e)}
         />
       </label>
     </>
